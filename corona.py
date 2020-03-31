c45252bb70dfa9ec167db84fbdb423b7078e64ef
sandeep501 <sandeepanmi1997@gmail.com>
2020-03-31 16:13:47 +0530

    corona Api

---
+++ corona.py
@@ -0,0 +1,19 @@
+import requests
+
+url='https://pomber.github.io/covid19/timeseries.json'
+
+data=(requests.get(url)).json()
+
+data=data['India'][-1]
+
+date=data['date']
+
+confirmed=data['confirmed']
+
+recovered=data['recovered']
+
+deaths=data['deaths']
+
+print('As of {0} confirmed Corona cases in India are {1}'.format(date,confirmed))
+
+print('out of this {0} were recovered and {1} are dead'.format(recovered,deaths))
